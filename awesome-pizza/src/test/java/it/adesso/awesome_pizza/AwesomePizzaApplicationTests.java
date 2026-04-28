ec3c9e4f0f364c99bd943d6f85dc8461e16f2a44
Matthew0298 <matteo.tropiano98@outlook.it>
2026-04-28 20:20:01 +0200

    spring setup start

---
+++ awesome-pizza/src/test/java/it/adesso/awesome_pizza/AwesomePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.adesso.awesome_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomePizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
